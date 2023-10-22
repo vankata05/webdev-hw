c909c249d7c645fc96c94817dd03f74e43303e9b
ivan <ivan.v.ivanov.2019@elsys-bg.org>
2023-10-22 15:39:22 +0300

    Done

--- chat/client/client.java
+++ chat/client/client.java
@@ -1,18 +1,58 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
-public class EchoClient {
-    try {
-        System.out.println("Client started");
-        Socket soc = new Socket("localhost", 9806);
-        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter a string");
-        String str = userInput.readLine();
-        PrintStream ps = new PrintStream(soc.getOutputStream());
-        ps.println(str);
-    }catch(Exception e){
-        e.printStackTrace();
+
+public class client {
+    static Socket soc;
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String usrIn = new String();
+        String nick = "generic"; //aka ivaylo
+        soc = null;
+        try {
+            while(true){
+                usrIn = br.readLine();
+                if(usrIn.startsWith("/connect ")){
+                    String[] ipPort = usrIn.split(" ");
+                    String ip = ipPort[1].split(":")[0];
+                    int port = Integer.parseInt(ipPort[1].split(":")[1]);
+                    soc = new Socket(ip, port);
+
+                    new Thread(() -> {
+                        try {
+                            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                            String receivedMsg;
+                            while ((receivedMsg = in.readLine()) != null) {
+                                System.out.println(receivedMsg);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+
+                }else if(usrIn.startsWith("/msg ") & soc != null){
+                    String[] msg = usrIn.split(" ");
+                    String message = msg[1];
+                    PrintStream out = new PrintStream(soc.getOutputStream());
+                    out.println(nick + ":" +message);
+
+                }else if(usrIn.startsWith("/nick ")){
+                    String[] name = usrIn.split(" ");
+                    nick = name[1];
+                    
+                }else if(usrIn.startsWith("/down")){
+                    System.exit(0);
+                }else{
+                    System.out.println("Invalid command");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
+
--- chat/server/server.java
+++ chat/server/server.java
@@ -1,23 +1,65 @@
-import Java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintStream;
+// import java.lang.reflect.Array;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class EchoServer {
+// /connect localhost:9806
+
+public class server {
+    volatile static ArrayList<BufferedReader> inList = new ArrayList<BufferedReader>();
+    volatile static ArrayList<PrintStream> outList = new ArrayList<PrintStream>();
+
     public static void main(String[] args) {
         try {
             System.out.println("Server started");
-            ServerSocket s = new ServerSocket(9806);
-            Socket soc = s.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            String str = in.readLine();
-            System.out.println("Client said: " + str);
-        } catch (Exception e) {
+            try (ServerSocket serverSocket = new ServerSocket(9806)) {
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                    inList.add(new BufferedReader(new InputStreamReader(clientSocket.getInputStream())));
+                    outList.add(new PrintStream(clientSocket.getOutputStream()));
+
+                    new Thread(() -> {
+                        try {
+                            while (true) {
+                                BufferedReader in = inList.get(inList.size() - 1);
+
+                                String clientMsg = in.readLine();
+                                if (clientMsg == null) {
+                                    break;
+                                }
+
+                                List<String> msg = new ArrayList<String>();
+                                for (String retval : clientMsg.split(":", 2)) {
+                                    msg.add(retval);
+                                }
+
+                                System.out.println("Received message from client " + msg.get(0) + "(" +clientSocket.getInetAddress().getHostAddress() + "): " + msg.get(1));
+                                
+                                for (PrintStream outStream : outList) {
+                                    if(in != inList.get(outList.indexOf(outStream))){
+                                        outStream.println(clientMsg);
+                                    }
+                                }
+                            }
+
+                            clientSocket.close();
+                            System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
-//command to install jdk on windows powershell
-// choco install jdk8
-// with admin rigths
-// choco install jdk8 -y
